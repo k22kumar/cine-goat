6b1a054709ec6ebde6e9b41659950eac5ac4ad1f
Kajanth Kumar <kajanthkumar@gmail.com>
2020-05-01 15:41:56 -0400

    Updates

---
+++ precache-manifest.22dd84892c1f23f707f3f6c39c779dd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4de1b9bce23ea7752a1033e09df95b",
+    "url": "/cine-goat/index.html"
+  },
+  {
+    "revision": "29637ac5c6bf616446f6",
+    "url": "/cine-goat/static/css/main.a2b4ee76.chunk.css"
+  },
+  {
+    "revision": "0a467c941f70d14cecc1",
+    "url": "/cine-goat/static/js/2.6e943056.chunk.js"
+  },
+  {
+    "revision": "4ae2e24854201e2c90f766e681fee893",
+    "url": "/cine-goat/static/js/2.6e943056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29637ac5c6bf616446f6",
+    "url": "/cine-goat/static/js/main.754b38a8.chunk.js"
+  },
+  {
+    "revision": "e2ddbb71b1097365d090",
+    "url": "/cine-goat/static/js/runtime-main.51a35cb2.js"
+  },
+  {
+    "revision": "75e38e063d45b7d98992a2da8310a77d",
+    "url": "/cine-goat/static/media/temp.75e38e06.jpg"
+  }
+]);
