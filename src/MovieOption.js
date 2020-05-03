03369c623477a7ff2c999a6fdcc095e3b638715b
Kajanth Kumar <kajanthkumar@gmail.com>
2020-05-03 18:34:15 -0400

    added box shadows

--- src/MovieOption.js
+++ src/MovieOption.js
@@ -26,32 +26,32 @@ class MovieOption extends Component {
       // you can create some logic here like adding a variable and then instead of putting className put a variable
       // console.log("props", this.props);
         return (
-          <ul className="movieOptionContainer">
-            <li className="votes">
-              <p>Votes: {this.props.votes}</p>
-            </li>
-            <li className="movieImg">
-              <img
-                src={this.props.image}
-                alt={"Movie poster for: " +this.props.movieTitle}
-              />
-            </li>
-            <li className="likeAndDislike">
-              {/* put a turnerary that decides if it has been clicked then unclick dislike inside the button itself since it is JSX*/}
-              <button
-                className="like"
-                onClick={(e) => this.deferredLike(e, this.props.movieID)}
-              >
-                <i className="fas fa-thumbs-up"></i>
-              </button>
-              <button
-                className="dislike"
-                onClick={(e) => this.deferredDislike(e, this.props.movieID)}
-              >
-                <i className="fas fa-thumbs-down"></i>
-              </button>
-            </li>
-          </ul>
+          <article className="movieOptionContainer">
+              <p className="votes">Votes: {this.props.votes}</p>
+            <ul className="imgAndLikes">
+              <li className="movieImg">
+                <img
+                  src={this.props.image}
+                  alt={"Movie poster for: " + this.props.movieTitle}
+                />
+              </li>
+              <li className="likeAndDislike">
+                {/* put a turnerary that decides if it has been clicked then unclick dislike inside the button itself since it is JSX*/}
+                <button
+                  className="like"
+                  onClick={(e) => this.deferredLike(e, this.props.movieID)}
+                >
+                  <i className="fas fa-thumbs-up"></i>
+                </button>
+                <button
+                  className="dislike"
+                  onClick={(e) => this.deferredDislike(e, this.props.movieID)}
+                >
+                  <i className="fas fa-thumbs-down"></i>
+                </button>
+              </li>
+            </ul>
+          </article>
         );
     }
 
